efb7f92efecfc79bcfcf949cd21b98af34a0a594
Olawale Arowojolu <arolexwalex33@gmail.com>
2023-01-23 06:22:20 -0800

    python exception

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -16,19 +16,19 @@ size_t b, i;
 char *str;
 
 setbuf(stdout, NULL);
-printf("[.] bytes object info\n");
+printf([.] bytes object infon);
 if (PyBytes_Check(p) == 0)
 {
-printf("  [ERROR] Invalid Bytes Object\n");
+printf( [ERROR] Invalid Bytes Objectn);
 return;
 }
 str = ((PyBytesObject *)(p))->ob_sval, b = PyBytes_Size(p);
-printf("  size: %ld\n  trying string: %s\n", b, str);
+printf( size: %ldn trying string: %sn, b, str);
 b >= 10 ? b = 10 : b++;
-printf("  first %ld bytes: ", b);
+printf( first %ld bytes: , b);
 for (i = 0; i < b - 1; i++)
-printf("%02hhx ", str[i]);
-printf("%02hhx\n", str[i]);
+printf(%02hhx , str[i]);
+printf(%02hhxn, str[i]);
 }
 /**
  * print_python_float - print python things
@@ -40,15 +40,15 @@ char *str;
 double f;
 
 setbuf(stdout, NULL);
-printf("[.] float object info\n");
+printf([.] float object infon);
 if (PyFloat_Check(p) == 0)
 {
-printf("  [ERROR] Invalid Float Object\n");
+printf( [ERROR] Invalid Float Objectn);
 return;
 }
 f = ((PyFloatObject *)(p))->ob_fval;
-str = PyOS_double_to_string(f, r, 0, Py_DTSF_ADD_DOT_0, NULL);
-printf("  value: %s\n", str);
+str = PyOS_double_to_string(f, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+printf( value: %sn, str);
 }
 /**
  * print_python_list - print python things
@@ -61,21 +61,19 @@ const char *t;
 PyListObject *list;
 
 setbuf(stdout, NULL);
-printf("[*] Python list info\n");
+printf([*] Python list infon);
 if (PyList_Check(p) == 0)
 {
-printf("  [ERROR] Invalid List Object\n");
+printf( [ERROR] Invalid List Objectn);
 return;
 }
 list = (PyListObject *)p;
 size = PyList_GET_SIZE(p);
 a = list->allocated;
-printf("[*] Size of the Python List = %ld\n[*] Allocated = %li\n", size, a);
+printf([*] Size of the Python List = %ldn[*] Allocated = %lin, size, a);
 for (i = 0; i < size; i++)
 {
 t = (list->ob_item[i])->ob_type->tp_name;
-printf("Element %li: %s\n", i, t);
-!strcmp(t, "bytes") ? print_python_bytes(list->ob_item[i]) : (void)t;
-!strcmp(t, "float") ? print_python_float(list->ob_item[i]) : (void)t;
+printf(Element %li: %sn, i, t);
 }
 }
